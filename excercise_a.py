5ecfc552917cfc40830d599d6b126a014bcde1bc
Mathias Kerr <mathiasfkerr@gmail.com>
2023-01-10 16:39:14 +0000

    Excercise a completed

--- excercise_a.py
+++ excercise_a.py
@@ -1,7 +1,33 @@
 
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
-
-#1. Add "Edinburgh Waverley" to the end of the list
+#1
+stops.append('Edinburgh Waverly')
+print(stops)
+#2
+stops.insert(0, "Glasgow Queen St")
+print(stops)
+#3
+stops.insert(4, "Polmont")
+print(stops)
+#4
+print(stops.index("Linlithgow"))
+#5
+stops.pop(6)
+print(stops)
+#6
+stops.pop(2)
+print(stops)
+#7
+print(len(stops))
+#8
+print(sorted(stops))
+#9
+stops.sort(reverse = True)
+print(stops)
+#10
+for stop in stops:
+    print(stop)
+#1. Add "Edinburgh Waverley" to the end of the list 
 #2. Add "Glasgow Queen St" to the start of the list
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
 #4. Print out the index position of "Linlithgow"
